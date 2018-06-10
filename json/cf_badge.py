bc5b4d7ac2d90f7c33cfd3b9280810fac24d1442
nyon <clarinet758@gmail.com>
2018-06-10 14:59:41 +0900

    update
    2 to 3

--- json/cf_badge.py
+++ json/cf_badge.py
@@ -1,85 +1,100 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-import time
-import sys
-import io
-import re
+#!/usr/bin/env python3
+
 import datetime
+import urllib.request, urllib.error
 import json
 
-n=''
-filename="./data.json"
+#0:テスト 1:本番 shellから受け取り
+i = int(input())
 
-info='./info.json'
-rating='./rating.json'
-status='./status.json'
-savedata="./data.json"
-
-def jsonload(filename):
-    f=open(filename)
-    data=json.load(f)
-    f.close()
-    return data
-
-data1=jsonload(info)
-data2=jsonload(rating)
-data3=jsonload(status)
-master=jsonload(savedata)  
+#m = ["テスト", "本番"]
+#print(m[i]+"で動作中")
+#exit(1)
 
 
-# can not get 'OK',do not update.  because do not know any time to recover.
-if data1['status']!='OK': exit()
-if data2['status']!='OK': exit()
-if data3['status']!='OK': exit()
+us = "clarinet758"
+url   = "http://codeforces.com/api/user."
+url_i = url + "info?handles=" + us
+url_r = url + "rating?handle=" + us
+url_s = url + "status?handle=" + us + "&from=1&count=1"
 
-info=data1['result'][0]
-cont=data2['result'][-1]
-subm=data3['result'][0]
+if i == 0:
+    p  = "/home/reina/magic/cold/cf_badge/data.json"
 
-master['handle']=n
-master['rankName']=info['rank']
-master['maxRankName']=info['maxRank']
-master['rating']=info['rating']
-master['maxRating']=info['maxRating']
-master['contestId']=str(cont['contestId'])
-master['contestName']=cont['contestName']
-master['contestRank']=cont['rank']
-flu=cont['newRating']-cont['oldRating']
-if flu>0:
-    flu='+'+str(flu)
-elif flu>=0:
-    flu=str(flu)
-master['fluctuation']=flu
-master['fluctuation']=flu
-dt=datetime.datetime.fromtimestamp(subm['creationTimeSeconds'])
-if dt.month>9:
-    mo=str(dt.month)
+elif i == 1:
+    p  = "/var/www/html/joso/data.json"
+
 else:
-    mo=('0'+str(dt.month))
-if dt.day>9:
-    dy=str(dt.day)
-else:
-    dy=('0'+str(dt.day))
-if dt.hour>9:
-    ho=str(dt.hour)
-else:
-    ho=('0'+str(dt.hour))
-if dt.minute>9:
-    mn=str(dt.minute)
-else:
-    mn=('0'+str(dt.minute))
-if dt.second>9:
-    sc=str(dt.second)
-else:
-    sc=('0'+str(dt.second))
-ts=str(dt.year)+'-'+mo+'-'+dy+' '+ho+':'+mn+':'+sc
-master['when']=ts
-if subm['verdict']=='OK':
-    subm['verdict']='Accepted'
-master['verdict']=subm['verdict']
-master['submitContestId']=str(subm['problem']['contestId'])
-master['submitCodeId']=str(subm['id'])
-master['index']=(subm['problem']['index'])
-master['problemName']=(subm['problem']['name'])
+    exit(1)
 
-json.dump(master, open(savedata, 'w'))
+f   = open(p)
+mas = json.load(f)
+f.close()
+#print(mas)
+
+def gj(url):
+    res = urllib.request.urlopen(url)
+    nama = res.read()
+    nama8 = nama.decode("utf_8")
+    j = json.loads(nama8)
+    return j
+
+def zero(s):
+    if s//10:
+        return str(s)
+    else:
+        return "0" + str(s)
+
+error   = {"status": "ERROR"}
+info    = gj(url_i)
+rating  = gj(url_r)
+status  = gj(url_s)
+
+#3種のjsonがOKで用意できた場合には処理を続ける
+if ("OK" == info["status"] == rating["status"] == status["status"]):
+#if ("OK" == info["status"] == rating["status"] == status["status"] == error["status"]):
+    #print("OK")
+    pass
+else:
+    exit(1)
+
+i = info["result"][0]
+r = rating["result"][-1]
+s = status["result"][0]
+
+#コンテスト結果
+mas["rankName"]    = i["rank"]
+mas["maxRankName"] = i["maxRank"]
+mas["rating"]      = i["rating"]
+mas["maxRating"]   = i["maxRating"]
+mas["contestId"]   = str(r["contestId"])
+mas["contestName"] = r["contestName"]
+mas["contestRank"] = r["rank"]
+flu                = r["newRating"] - r["oldRating"]
+
+if flu > 0:
+    flu = "+"+str(flu)
+else:
+    flu = str(flu)
+mas["fluctuation"] = flu
+
+dt = datetime.datetime.fromtimestamp(s["creationTimeSeconds"])
+mo = zero(dt.month)
+dy = zero(dt.day)
+ho = zero(dt.hour)
+mn = zero(dt.minute)
+sc = zero(dt.second)
+
+ts = str(dt.year)+"-"+mo+"-"+dy+" "+ho+":"+mn+":"+sc
+mas["when"] = ts
+
+if s["verdict"] == "OK":
+    s["verdict"] = "Accepted"
+mas["verdict"]         = s["verdict"]
+mas["submitContestId"] = str(s["problem"]["contestId"])
+mas["submitCodeId"]    = str(s["id"])
+mas["index"]           = str(s["problem"]["index"])
+mas["problemName"]     = s["problem"]["name"]
+#print(mas)
+
+json.dump(mas, open(p, 'w'))
